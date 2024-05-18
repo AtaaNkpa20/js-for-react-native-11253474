c32da5b49190de63144ce2afda7ddef7d81fc8f4
Raphael Quaye <rquaye023@st.ug.edu.gh>
2024-05-18 20:49:21 +0100

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,9 +10,24 @@ function proccesArray(arr) {
     });
 }
 
+// Function to format an array of strings based on corresponding array of numbers
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
 
 //Example usage 
 const inputArray = [6, 7, 8, 9, 10];
 const resultArray = proccesArray(inputArray);
 
+const stringArray = ["good", "bad", "watch", "fun", "javascript"];
+const formattedStrings = formatArrayStrings(stringArray, resultArray);
+
 console.log(resultArray);
+console.log(formattedStrings);
